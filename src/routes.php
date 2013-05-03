8e8fd4d48407e0f8291e4437874264a117ee4a65
Nils Werner <nils.werner@gmail.com>
2013-05-03 16:20:43 +0200

    Split up routes so we can give them names

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,34 @@
 <?php
 
-Route::controller('scaffold/{args}', 'NilsWerner\Scaffold\ScaffoldController');
+Route::group(array('prefix' => 'scaffold'), function()
+{
+	Route::get('{model}', array(
+		'as' => 'scaffold.index',
+		'uses' => 'NilsWerner\Scaffold\ScaffoldController@getIndex'
+		));
+
+	Route::get('{model}/create', array(
+		'as' => 'scaffold.create',
+		'uses' => 'NilsWerner\Scaffold\ScaffoldController@getCreate'
+		));
+
+	Route::post('{model}', array(
+		'as' => 'scaffold.store',
+		'uses' => 'NilsWerner\Scaffold\ScaffoldController@postIndex'
+		));
+
+	Route::get('{model}/{id}/edit', array(
+		'as' => 'scaffold.edit',
+		'uses' => 'NilsWerner\Scaffold\ScaffoldController@getEdit'
+		));
+
+	Route::post('{model}/{id}', array(
+		'as' => 'scaffold.update',
+		'uses' => 'NilsWerner\Scaffold\ScaffoldController@postEdit'
+		));
+
+	Route::post('{model}/{id}/delete', array(
+		'as' => 'scaffold.delete',
+		'uses' => 'NilsWerner\Scaffold\ScaffoldController@postDelete'
+		));
+});
